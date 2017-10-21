266b51aa057b22e91fb3b5649efce9e538b0b3f7
bhagyar <rathnayake.bhagya94@gmail.com>
2017-10-21 12:27:14 +0530

    Recent Updates

--- app/src/main/java/business_modules/map_view/MapViewActivity.java
+++ app/src/main/java/business_modules/map_view/MapViewActivity.java
@@ -20,7 +20,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
@@ -53,13 +57,12 @@ public class MapViewActivity extends FragmentActivity
             @Override
             public void onClick(View view) {
                 LatLng currentLocation = new LatLng(latitude, longitude);
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 19.2f), 1000, null);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15.2f), 1000, null);
                 mMap.clear();
                 mMap.addMarker(new MarkerOptions()
                         .position(currentLocation)
                 );
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-
                 DatabaseReference myRef = database.getReference("userCoordinates");
                 myRef.child(deviceId).setValue(currentLocation);
                 Log.i("Location Updated", "UPDATED AND COMMITTED TO FIREBASE");
@@ -71,52 +74,48 @@ public class MapViewActivity extends FragmentActivity
         mapFragment.getMapAsync(this);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
 
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    latitude = location.getLatitude();
-                    longitude = location.getLongitude();
-                    Geocoder geocoder = new Geocoder(getApplicationContext());
-                    try {
-//                        List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
-//                        String str = addressList.get(0).getLocality() + ",";
-//                        str += addressList.get(0).getCountryName();
-                        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
-                    } catch (Exception e) {
-                        Log.e("EXCEPTION>>>>>>>>>>>>", "" + e);
-                    }
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                LatLng currentLocation = new LatLng(latitude, longitude);
+                mMap.clear();
+                mMap.setTrafficEnabled(true);
+                Marker marker = mMap.addMarker(new MarkerOptions()
+                        .position(currentLocation)
+                        .title("You Are Here")
+                        .snippet("You Location is Visible to AKOYA")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_person_pin_circle_green_600_24dp))
+                );
+                marker.showInfoWindow();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("userCoordinates");
+                myRef.child(deviceId).setValue(currentLocation);
+                Log.i("Location Updated", "UPDATED AND COMMITTED TO FIRE-BASE");
+            }
 
-                }
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
 
-                @Override
-                public void onStatusChanged(String s, int i, Bundle bundle) {
+            }
 
-                }
+            @Override
+            public void onProviderEnabled(String s) {
 
-                @Override
-                public void onProviderEnabled(String s) {
+            }
 
-                }
+            @Override
+            public void onProviderDisabled(String s) {
 
-                @Override
-                public void onProviderDisabled(String s) {
+            }
+        };
 
-                }
-            });
-        }
-
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
     }
 
@@ -133,15 +132,10 @@ public class MapViewActivity extends FragmentActivity
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 19.2f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15.2f));
         mMap.addMarker(new MarkerOptions()
                 .position(currentLocation)
         );
-
-
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("You Are Here"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 }
